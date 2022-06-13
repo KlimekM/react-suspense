4a98ebd45256ea4ccda67b6dcb48520a100b0366
Michal Klimek <mklimek15@gmail.com>
2022-06-13 09:33:04 -0500

    01-EC1: complete "Simple Data-Fetching - Handle Error with Error Boundary"

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,20 +2,25 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-import {fetchPokemon, PokemonDataView} from '../pokemon'
+import {fetchPokemon, PokemonDataView, PokemonErrorBoundary} from '../pokemon'
 
 let pokemon
+let pokemonError
 // We don't need the app to be mounted to know that we want to fetch the pokemon
 // named "pikachu" so we can go ahead and do that right here.
-const pokemonPromise = fetchPokemon('pikachu').then(
+const pokemonPromise = fetchPokemon('pikacha').then(
   pokemonData => (pokemon = pokemonData),
+  error => (pokemonError = error),
 )
 
 function PokemonInfo() {
+  if (pokemonError) {
+    throw pokemonError
+  }
+
   if (!pokemon) {
     throw pokemonPromise
   }
-
   // if the code gets it this far, then the pokemon variable is defined and
   // rendering can continue!
   return (
@@ -32,9 +37,11 @@ function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        <React.Suspense fallback={<div>Loading...</div>}>
-          <PokemonInfo />
-        </React.Suspense>
+        <PokemonErrorBoundary>
+          <React.Suspense fallback={<div>Loading...</div>}>
+            <PokemonInfo />
+          </React.Suspense>
+        </PokemonErrorBoundary>
       </div>
     </div>
   )
