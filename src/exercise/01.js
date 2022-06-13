89f1014f0833b77b75c3ab83c41ef050cd32e2e8
Michal Klimek <mklimek15@gmail.com>
2022-06-13 09:42:49 -0500

    01-EC3: complete "Simple Data-Fetching - Use utils"

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,29 +2,13 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-import {fetchPokemon, PokemonDataView, PokemonErrorBoundary} from '../pokemon'
-
-function createResource(promise) {
-  let status = 'pending'
-  let result = promise.then(
-    resolved => {
-      status = 'success'
-      result = resolved
-    },
-    rejected => {
-      status = 'error'
-      result = rejected
-    },
-  )
-  return {
-    read() {
-      if (status === 'pending') throw result
-      if (status === 'error') throw result
-      if (status === 'success') return result
-      throw new Error('This should be impossible')
-    },
-  }
-}
+import {
+  fetchPokemon,
+  PokemonDataView,
+  PokemonErrorBoundary,
+  PokemonInfoFallback,
+} from '../pokemon'
+import {createResource} from '../utils'
 
 let pokemonResource = createResource(fetchPokemon('pikachu'))
 
@@ -47,7 +31,7 @@ function App() {
     <div className="pokemon-info-app">
       <div className="pokemon-info">
         <PokemonErrorBoundary>
-          <React.Suspense fallback={<div>Loading...</div>}>
+          <React.Suspense fallback={<PokemonInfoFallback name="Pikachu" />}>
             <PokemonInfo />
           </React.Suspense>
         </PokemonErrorBoundary>
